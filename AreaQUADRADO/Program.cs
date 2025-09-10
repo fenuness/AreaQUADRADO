b0885363fcf8583e1bd35fa68cf2953cd2f9c3a2
Felipe Nunes <felipe.nuns06@gmail.com>
2025-09-10 19:07:39 -0300

    Adicionar arquivos de projeto.

---
+++ AreaQUADRADO/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("escreva o lado do quadrado");
+string ladoTexto = Console.ReadLine();
+double lado = Convert.ToDouble(ladoTexto);
+double area = lado * lado;
+
+Console.WriteLine("A area do quadrado é " + area);
